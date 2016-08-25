333bad87aeca731018c56f8c18a5ae71adbc5616
geowarin <geowarin@gmail.com>
2016-08-25 13:02:25 +0200

    Add doc/comment for fomatters

--- src/core/formatErrors.js
+++ src/core/formatErrors.js
@@ -1,5 +1,10 @@
 /**
  * Applies formatters to all AnnotatedErrors.
+ *
+ * A formatter has the following signature FormattedError => Array<String>.
+ * It takes a formatted error produced by a transormer and returns a list
+ * of log statements to print.
+ *
  */
 function formatErrors(errors, formatters, errorType) {
   const format = (formatter) => formatter(errors, errorType) || [];
