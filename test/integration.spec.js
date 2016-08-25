6153ad2f29f13d7a09d5a972ae2ed9a89be79e5f
geowarin <geowarin@gmail.com>
2016-08-25 10:17:13 +0200

    Simplify tests with the latest version of babel-loader
    Babel-loader now does some work for us (cleaner errors).
    We could also remove the stack trace removal logic in the future:
    https://github.com/babel/babel-loader/releases/tag/v6.2.5
    
    For the moment, I think it's ok to keep it until most project switch to the newer version

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -33,7 +33,8 @@ test('integration : babel syntax error', t => {
     '',
     '1) Error in ./fixtures/babel-syntax/index.js',
     '',
-    `Module build failed: SyntaxError: ${__dirname}/fixtures/babel-syntax/index.js: Unexpected token (5:11)
+    `Module build failed: SyntaxError: Unexpected token (5:11)
+
   3 |${' '}
   4 |   render() {
 > 5 |     return <div>
