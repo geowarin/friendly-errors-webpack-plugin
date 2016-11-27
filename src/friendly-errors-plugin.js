8f2b90e5152e724fdfd0b602e9703b800b8b12ee
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-11-27 05:07:33 -0400

    Fix clearConsole option not working when set to false (#10)

--- src/friendly-errors-plugin.js
+++ src/friendly-errors-plugin.js
@@ -28,7 +28,7 @@ class FriendlyErrorsWebpackPlugin {
     options = options || {};
     this.compilationSuccessInfo = options.compilationSuccessInfo || {};
     this.onErrors = options.onErrors;
-    this.shouldClearConsole = options.clearConsole || true;
+    this.shouldClearConsole = Boolean(options.clearConsole);
     this.formatters = concat(defaultFormatters, options.additionalFormatters);
     this.transformers = concat(defaultTransformers, options.additionalTransformers);
   }
