4c5bf975d4c6f4f735fe73f6d5a07724de73f448
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-03-13 03:02:22 +0100

    add strip-ansi to cover for removed stripColor function in chalk

--- test/unit/formatters/defaultError.spec.js
+++ test/unit/formatters/defaultError.spec.js
@@ -1,8 +1,8 @@
 const defaultError = require('../../../src/formatters/defaultError');
 const expect = require('expect');
-const chalk = require('chalk');
+const stripAnsi = require('strip-ansi');
 
-const noColor = (arr) => arr.map(chalk.stripColor);
+const noColor = (arr) => arr.map(stripAnsi);
 const error = { message: 'Error message', file: './src/index.js' };
 
 it('Formats errors with no type', () => {
